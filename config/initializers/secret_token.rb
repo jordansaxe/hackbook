9c524a819614195e44d9aec3f9cee32222dc2035
Jordan Saxe <jordansaxe15@gmail.com>
2014-04-07 18:48:48 -0400

    Create initial app and add email validation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackbook::Application.config.secret_key_base = '48fb5c6e9e19ac8cd92e24ddcbb07ca123dbd5525044de554fa253cbf64e3f46daeae39af9834145d29be7114ff2840992a526559edaf0fef055b12866a19807'

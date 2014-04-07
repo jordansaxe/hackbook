bf93bb39a6ce0a2809f0c882e26f4463710bd3dd
Jordan Saxe <jordansaxe15@gmail.com>
2014-04-07 19:18:29 -0400

    Add date of birth validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
+  validates :date_of_birth, presence: true
+  validate :is_18_years_old
+end
+
+def is_18_years_old
+  date = 18.years.ago
+  if date_of_birth > date
+    errors.add(:date_of_birth, "You need to be 18 years old to register")
+  end
 end
